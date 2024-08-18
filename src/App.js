39de7a0d03725492483cc35dabc6f7b40a46a8bd
0MadGaming0 <madhavsp05@gmail.com>
2024-08-18 12:53:59 +0530

    Shopping Cart

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddProducts from './components/AddProducts';
+import SearchProducts from './components/SearchProducts';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<AddProducts/>}/>
+      <Route path="/search" element={<SearchProducts/>}/>
     </Routes>
     </BrowserRouter>
   );
